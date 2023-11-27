8bddba6c1494c987c67e404bdbd272f005f22207
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2023-11-27 20:26:04 +0900

    直接SoundEventを変数代入するように設定。 Forgeサーバでも音声が差し替えられるようになった。

--- src/main/java/net/yokohama_miyazawa/maidillager/MaidIllager.java
+++ src/main/java/net/yokohama_miyazawa/maidillager/MaidIllager.java
@@ -17,7 +17,6 @@ import net.yokohama_miyazawa.maidillager.init.MaidIllagerSounds;
 import org.slf4j.Logger;
 
 import java.util.Objects;
-import java.util.Optional;
 import java.util.regex.Pattern;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -33,8 +32,6 @@ public class MaidIllager
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
-
-        MaidIllagerSounds.SOUND_EVENTS.register(modEventBus);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -58,13 +55,9 @@ public class MaidIllager
                     event.getSound(),
                     "Original sound must not be null.");
             String soundId = sound.get().getLocation().toString().split(":")[1];
-            if (isIllager(soundId) && isIllagerTalkEvent(soundId)) {
-                getMaidSoundEventHolder(soundId).ifPresentOrElse(v -> {
-                    event.setNewPitch(1.0f);
-                    event.setSound(v);
-                }, () -> {
-                    LOGGER.warn("MaidSoundEventHolder is null. Nothing done.");
-                });
+            if (isIllager(soundId) && isIllagerTalkEvent(soundId)) {  // 邪悪な村人が喋る時のみ音声を差し替える
+                event.setNewPitch(1.0f);
+                event.setSound(Holder.direct(getMaidSoundEvent(soundId)));
             }
         }
     }
@@ -77,25 +70,25 @@ public class MaidIllager
         return id.endsWith("ambient") || id.endsWith("celebrate") || id.endsWith("death") || id.endsWith("hurt");
     }
 
-    private Optional<Holder<SoundEvent>> getMaidSoundEventHolder(String soundId) {
+    private SoundEvent getMaidSoundEvent(String soundId) {
         String[] ids = soundId.split(Pattern.quote("."));
         String mobAction = String.join(".", ids[1], ids[2]);
         switch(mobAction){
-            case "evoker.ambient"       -> { return MaidIllagerSounds.MAID_EVOKER_AMBIENT.getHolder(); }
-            case "evoker.celebrate"     -> { return MaidIllagerSounds.MAID_EVOKER_CELEBRATE.getHolder(); }
-            case "evoker.death"         -> { return MaidIllagerSounds.MAID_EVOKER_DEATH.getHolder(); }
-            case "evoker.hurt"          -> { return MaidIllagerSounds.MAID_EVOKER_HURT.getHolder(); }
-            case "illusioner.ambient"   -> { return MaidIllagerSounds.MAID_ILLUSIONER_AMBIENT.getHolder(); }
-            case "illusioner.death"     -> { return MaidIllagerSounds.MAID_ILLUSIONER_DEATH.getHolder(); }
-            case "illusioner.hurt"      -> { return MaidIllagerSounds.MAID_ILLUSIONER_HURT.getHolder(); }
-            case "pillager.ambient"     -> { return MaidIllagerSounds.MAID_PILLAGER_AMBIENT.getHolder(); }
-            case "pillager.celebrate"   -> { return MaidIllagerSounds.MAID_PILLAGER_CELEBRATE.getHolder(); }
-            case "pillager.death"       -> { return MaidIllagerSounds.MAID_PILLAGER_DEATH.getHolder(); }
-            case "pillager.hurt"        -> { return MaidIllagerSounds.MAID_PILLAGER_HURT.getHolder(); }
-            case "vindicator.ambient"   -> { return MaidIllagerSounds.MAID_VINDICATOR_AMBIENT.getHolder(); }
-            case "vindicator.celebrate" -> { return MaidIllagerSounds.MAID_VINDICATOR_CELEBRATE.getHolder(); }
-            case "vindicator.death"     -> { return MaidIllagerSounds.MAID_VINDICATOR_DEATH.getHolder(); }
-            case "vindicator.hurt"      -> { return MaidIllagerSounds.MAID_VINDICATOR_HURT.getHolder(); }
+            case "evoker.ambient"       -> { return MaidIllagerSounds.MAID_EVOKER_AMBIENT; }
+            case "evoker.celebrate"     -> { return MaidIllagerSounds.MAID_EVOKER_CELEBRATE; }
+            case "evoker.death"         -> { return MaidIllagerSounds.MAID_EVOKER_DEATH; }
+            case "evoker.hurt"          -> { return MaidIllagerSounds.MAID_EVOKER_HURT; }
+            case "illusioner.ambient"   -> { return MaidIllagerSounds.MAID_ILLUSIONER_AMBIENT; }
+            case "illusioner.death"     -> { return MaidIllagerSounds.MAID_ILLUSIONER_DEATH; }
+            case "illusioner.hurt"      -> { return MaidIllagerSounds.MAID_ILLUSIONER_HURT; }
+            case "pillager.ambient"     -> { return MaidIllagerSounds.MAID_PILLAGER_AMBIENT; }
+            case "pillager.celebrate"   -> { return MaidIllagerSounds.MAID_PILLAGER_CELEBRATE; }
+            case "pillager.death"       -> { return MaidIllagerSounds.MAID_PILLAGER_DEATH; }
+            case "pillager.hurt"        -> { return MaidIllagerSounds.MAID_PILLAGER_HURT; }
+            case "vindicator.ambient"   -> { return MaidIllagerSounds.MAID_VINDICATOR_AMBIENT; }
+            case "vindicator.celebrate" -> { return MaidIllagerSounds.MAID_VINDICATOR_CELEBRATE; }
+            case "vindicator.death"     -> { return MaidIllagerSounds.MAID_VINDICATOR_DEATH; }
+            case "vindicator.hurt"      -> { return MaidIllagerSounds.MAID_VINDICATOR_HURT; }
             default -> { throw new IllegalArgumentException(); }
         }
     }
--- src/main/java/net/yokohama_miyazawa/maidillager/init/MaidIllagerSounds.java
+++ src/main/java/net/yokohama_miyazawa/maidillager/init/MaidIllagerSounds.java
@@ -2,33 +2,27 @@ package net.yokohama_miyazawa.maidillager.init;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import net.yokohama_miyazawa.maidillager.MaidIllager;
 
 public class MaidIllagerSounds {
-    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
-            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MaidIllager.MODID);
+    public static final SoundEvent MAID_EVOKER_AMBIENT = createEvent("entity.evoker.ambient");
+    public static final SoundEvent MAID_EVOKER_CELEBRATE = createEvent("entity.evoker.celebrate");
+    public static final SoundEvent MAID_EVOKER_DEATH = createEvent("entity.evoker.death");
+    public static final SoundEvent MAID_EVOKER_HURT = createEvent("entity.evoker.hurt");
+    public static final SoundEvent MAID_ILLUSIONER_AMBIENT = createEvent("entity.illusioner.ambient");
+    public static final SoundEvent MAID_ILLUSIONER_DEATH = createEvent("entity.illusioner.death");
+    public static final SoundEvent MAID_ILLUSIONER_HURT = createEvent("entity.illusioner.hurt");
+    public static final SoundEvent MAID_PILLAGER_AMBIENT = createEvent("entity.pillager.ambient");
+    public static final SoundEvent MAID_PILLAGER_CELEBRATE = createEvent("entity.pillager.celebrate");
+    public static final SoundEvent MAID_PILLAGER_DEATH = createEvent("entity.pillager.death");
+    public static final SoundEvent MAID_PILLAGER_HURT = createEvent("entity.pillager.hurt");
+    public static final SoundEvent MAID_VINDICATOR_AMBIENT = createEvent("entity.vindicator.ambient");
+    public static final SoundEvent MAID_VINDICATOR_CELEBRATE = createEvent("entity.vindicator.celebrate");
+    public static final SoundEvent MAID_VINDICATOR_DEATH = createEvent("entity.vindicator.death");
+    public static final SoundEvent MAID_VINDICATOR_HURT = createEvent("entity.vindicator.hurt");
 
-    public static final RegistryObject<SoundEvent> MAID_EVOKER_AMBIENT = createEvent("entity.evoker.ambient");
-    public static final RegistryObject<SoundEvent> MAID_EVOKER_CELEBRATE = createEvent("entity.evoker.celebrate");
-    public static final RegistryObject<SoundEvent> MAID_EVOKER_DEATH = createEvent("entity.evoker.death");
-    public static final RegistryObject<SoundEvent> MAID_EVOKER_HURT = createEvent("entity.evoker.hurt");
-    public static final RegistryObject<SoundEvent> MAID_ILLUSIONER_AMBIENT = createEvent("entity.illusioner.ambient");
-    public static final RegistryObject<SoundEvent> MAID_ILLUSIONER_DEATH = createEvent("entity.illusioner.death");
-    public static final RegistryObject<SoundEvent> MAID_ILLUSIONER_HURT = createEvent("entity.illusioner.hurt");
-    public static final RegistryObject<SoundEvent> MAID_PILLAGER_AMBIENT = createEvent("entity.pillager.ambient");
-    public static final RegistryObject<SoundEvent> MAID_PILLAGER_CELEBRATE = createEvent("entity.pillager.celebrate");
-    public static final RegistryObject<SoundEvent> MAID_PILLAGER_DEATH = createEvent("entity.pillager.death");
-    public static final RegistryObject<SoundEvent> MAID_PILLAGER_HURT = createEvent("entity.pillager.hurt");
-    public static final RegistryObject<SoundEvent> MAID_VINDICATOR_AMBIENT = createEvent("entity.vindicator.ambient");
-    public static final RegistryObject<SoundEvent> MAID_VINDICATOR_CELEBRATE = createEvent("entity.vindicator.celebrate");
-    public static final RegistryObject<SoundEvent> MAID_VINDICATOR_DEATH = createEvent("entity.vindicator.death");
-    public static final RegistryObject<SoundEvent> MAID_VINDICATOR_HURT = createEvent("entity.vindicator.hurt");
-
-    private static RegistryObject<SoundEvent> createEvent(String sound) {
+    private static SoundEvent createEvent(String sound) {
         ResourceLocation name = new ResourceLocation(MaidIllager.MODID, sound);
-        return SOUND_EVENTS.register(sound, () -> SoundEvent.createVariableRangeEvent(name));
+        return SoundEvent.createVariableRangeEvent(name);
     }
 }
